aef325d4cdd950d2851c6ee78e4715e28ee27c13
Immanuel Paul <immanuelpaul7@gmail.com>
2019-04-25 12:56:10 -0700

    Third Commit

--- src/test/java/com/sndevops/eng/AppTest.java
+++ src/test/java/com/sndevops/eng/AppTest.java
@@ -5,10 +5,10 @@ import org.junit.Test;
 public class AppTest {
 // rrt   
     // Test
-    private String INPUT = "1324111";
+    private String INPUT = "13234111";
     @Test 
     public void testLength() { 
-        Assert.assertEquals(INPUT,"1324111");  
+        Assert.assertEquals(INPUT,"13234111");  
     }
 }
  
